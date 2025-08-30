17690826eb3ebf3ad9b0d06ba20e28f9826e5ebf
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-08-30 05:55:05 +0300

    build: add chakra components examples

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,40 @@
+'use client'
+
+import { Button } from '@chakra-ui/react'
 import Image from 'next/image'
 import type { JSX } from 'react'
+import { RxMagicWand } from 'react-icons/rx'
+import { ColorModeButton } from '@/components/ui/color-mode'
+import { toaster } from '@/components/ui/toaster'
+import { Tooltip } from '@/components/ui/tooltip'
 
 export default function Home(): JSX.Element {
+  const handleButtonClick = () => {
+    toaster.create({
+      title: 'Congrats! You are a fish',
+      type: 'success',
+      duration: 3000,
+      closable: true,
+    })
+  }
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <h1 className="font-bold text-4xl text-emerald-700">No No No Mister Fish</h1>
+    <div className="flex min-h-screen flex-col place-content-center gap-10">
+      <div className="fixed top-4 right-4 z-10">
+        <ColorModeButton />
+      </div>
+
+      <main className="flex place-content-center">
+        <div className="flex flex-col items-center gap-[32px] sm:items-start">
+          <h1 className="font-bold text-4xl text-emerald-700 dark:text-blue-400">No No No Mister Fish</h1>
+
+          <Tooltip content="Click Me" showArrow>
+            <Button onClick={handleButtonClick} colorPalette="green" size="lg" variant="surface">
+              Click Me! <RxMagicWand />
+            </Button>
+          </Tooltip>
+        </div>
       </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
+      <footer className="mb-6 flex flex-wrap place-content-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://app.rs.school/"
---
+++ components/ui/provider.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { ChakraProvider, ClientOnly, defaultSystem } from '@chakra-ui/react'
+import type { JSX } from 'react'
+import { ColorModeProvider, type ColorModeProviderProps } from './color-mode'
+
+export function Provider(props: ColorModeProviderProps): JSX.Element {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <ClientOnly>
+        <ColorModeProvider {...props} />
+      </ClientOnly>
+    </ChakraProvider>
+  )
+}
