eacb94828a729f4a7773efa246f49460a1a17a89
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-08-30 23:07:19 +0300

    chore: update configs

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
