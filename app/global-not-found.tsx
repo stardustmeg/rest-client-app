01c143e39813b394ec6e228f44878ef035698864
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-08-31 00:00:06 +0300

    feat: add global not found page

---
+++ app/global-not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import { useTranslations } from 'next-intl'
+import type { JSX } from 'react'
+
+import './main.css'
+
+export default function GlobalNotFound(): JSX.Element {
+  const t = useTranslations('global-not-found')
+
+  return (
+    <html lang="en">
+      <body>
+        <div>{t('title')}</div>
+        <Link href="/">{t('link')}</Link>
+      </body>
+    </html>
+  )
+}
