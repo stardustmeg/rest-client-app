6430f7b55eb385a3b0e8220004e4459c60c1d0e9
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-09-02 00:35:43 +0500

    feat: update content of GlobalNotFound page

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
 import './main.css';
+import Link from 'next/link';
 
 export const GlobalNotFound = () => {
   const t = useTranslations('global-not-found');
@@ -9,8 +9,20 @@ export const GlobalNotFound = () => {
   return (
     <html lang="en">
       <body>
-        <div>{t('title')}</div>
-        <Link href="/">{t('link')}</Link>
+        <div className="flex h-screen flex-col items-center justify-center gap-5">
+          <div className="flex text-9xl">
+            <div>4️⃣</div>
+            <div>0️⃣</div>
+            <div>4️⃣</div>
+          </div>
+          <h1 className="text-7xl">Page not found</h1>
+          <Link
+            className="rounded-2xl bg-[#5f889b] p-4 text-2xl transition hover:bg-[#427687]"
+            href="/"
+          >
+            {t('link')}
+          </Link>
+        </div>
       </body>
     </html>
   );
