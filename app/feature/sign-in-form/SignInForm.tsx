4ebf118228bc7340214af2f615a847fd1d226003
zagorky <zagorkiynahyi@gmail.com>
2025-09-01 20:14:14 +0300

    feat: update sign in form

--- app/feature/sign-in-form/SignInForm.tsx
+++ app/feature/sign-in-form/SignInForm.tsx
@@ -5,46 +5,48 @@ import { useTranslations } from 'next-intl';
 import { useForm } from 'react-hook-form';
 import { Enabled } from '@/app/components/ui/Enabled';
 import { FormField } from '@/app/components/ui/FormField';
-import { signInFormSchema } from '@/app/types/form-schemas';
-
-interface FormValues {
-  email: string;
-  password: string;
-}
+import { toaster } from '@/app/components/ui/Toaster';
+import { type SignInFormType, signInFormSchema } from '@/app/types/form-schemas';
 
 export const SignInForm = () => {
   const t = useTranslations('form');
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>({
+    formState: { errors, isValid, isSubmitting },
+  } = useForm<SignInFormType>({
     mode: 'onChange',
     reValidateMode: 'onChange',
     resolver: zodResolver(signInFormSchema),
   });
 
-  const onSubmit = (values: FormValues) => {
-    return values;
+  const onSubmit = () => {
+    toaster.create({
+      title: 'Form submitted',
+      type: 'success',
+      duration: 2000,
+      closable: true,
+    });
   };
 
   return (
     <Enabled feature={'signInForm'}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack>
+        <Stack maxW="lg" w="full" mx="auto" p="8">
           <Fieldset.Root>
-            <Fieldset.Legend>Sign In form</Fieldset.Legend>
-            <Fieldset.HelperText>Please provide your contact details below.</Fieldset.HelperText>
+            <Fieldset.Legend fontSize="xl" fontWeight="bold">
+              {t('signInTitle')}
+            </Fieldset.Legend>
           </Fieldset.Root>
-        </Stack>
-        <Stack gap="4" align="flex-start" maxW="lg">
           <FormField error={errors.email} label={t('email')} register={register('email')} />
           <FormField
             error={errors.password}
             label={t('password')}
             register={register('password')}
           />
-          <Button type="submit">{t('submit')}</Button>
+          <Button colorPalette="pink" w="full" disabled={!isValid || isSubmitting} type="submit">
+            {t('submit')}
+          </Button>
         </Stack>
       </form>
     </Enabled>
