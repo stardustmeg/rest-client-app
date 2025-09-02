b04114698c23e5217e159c2dfa785d23e31becc9
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-09-02 16:06:53 +0300

    feat: add navigation to global not found

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -1,28 +1,32 @@
-import { useTranslations } from 'next-intl';
+import { NextIntlClientProvider, useTranslations } from 'next-intl';
 
 import './main.css';
-import Link from 'next/link';
+import { Link } from '@/i18n/routing';
+import { Navigation } from './components/Navigation';
 
 export const GlobalNotFound = () => {
   const t = useTranslations('global-not-found');
 
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body>
-        <div className="flex h-screen flex-col items-center justify-center gap-5">
-          <div className="flex text-9xl">
-            <div>4️⃣</div>
-            <div>0️⃣</div>
-            <div>4️⃣</div>
+        <NextIntlClientProvider>
+          <Navigation />
+          <div className="flex h-screen flex-col items-center justify-center gap-5">
+            <div className="flex text-9xl">
+              <div>4️⃣</div>
+              <div>0️⃣</div>
+              <div>4️⃣</div>
+            </div>
+            <h1 className="text-7xl">{t('title')}</h1>
+            <Link
+              className="rounded-2xl bg-[#5f889b] p-4 text-2xl transition hover:bg-[#427687]"
+              href="/"
+            >
+              {t('link')}
+            </Link>
           </div>
-          <h1 className="text-7xl">{t('title')}</h1>
-          <Link
-            className="rounded-2xl bg-[#5f889b] p-4 text-2xl transition hover:bg-[#427687]"
-            href="/"
-          >
-            {t('link')}
-          </Link>
-        </div>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
