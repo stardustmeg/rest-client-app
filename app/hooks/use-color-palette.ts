7826084a68475ff76fe1518a7d72b997f2f5d65f
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-09-02 23:32:02 +0300

    feat: add color palette select

---
+++ app/hooks/use-color-palette.ts
@@ -0,0 +1,8 @@
+import { DEFAULT_COLOR_SCHEME } from '@/app/constants/color-theme';
+import { useColorScheme } from '@/app/hooks/use-color-scheme';
+
+export function useColorPalette() {
+  const { colorScheme } = useColorScheme();
+
+  return colorScheme ?? DEFAULT_COLOR_SCHEME;
+}
