347ee8678e47b8e1929bfb151994bbe32e5d0473
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-09-02 23:29:19 +0300

    feat: add color theme default values

---
+++ app/constants/color-theme.ts
@@ -0,0 +1,5 @@
+import type { ColorMode, ColorScheme } from '../types/color-theme';
+
+export const DEFAULT_COLOR_SCHEME: ColorScheme = 'purple';
+
+export const DEFAULT_COLOR_MODE: ColorMode = 'light';
