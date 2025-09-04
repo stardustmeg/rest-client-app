68ca7eea2f25162042c2b4d5084c550b9c92969d
zagorky <zagorkiynahyi@gmail.com>
2025-09-04 22:46:04 +0300

    feat: add tests

--- app/components/ui/__tests__/AboutDevelopersBlock.spec.tsx
+++ app/components/ui/__tests__/AboutDevelopersBlock.spec.tsx
@@ -7,16 +7,12 @@ describe('AboutDevelopersBlock', () => {
   it('renders all developer cards', async () => {
     const numberOfFields = 9;
     const element = await AboutDevelopersBlock();
-
     const html = renderToStaticMarkup(<TestProviders>{element}</TestProviders>);
-
     const cardMatches = html.match(/developers\./g) || [];
     expect(cardMatches.length).toBeGreaterThanOrEqual(numberOfFields);
-
     expect(html).toContain('developers.stardustmeg.author');
     expect(html).toContain('developers.ripetchor.author');
     expect(html).toContain('developers.zagorky.author');
-
     expect(html).toContain('https://avatars.githubusercontent.com/u/146496794?v=4');
     expect(html).toContain('https://avatars.githubusercontent.com/u/115036520?v=4');
     expect(html).toContain('https://avatars.githubusercontent.com/u/156232667?v=4');
@@ -25,11 +21,9 @@ describe('AboutDevelopersBlock', () => {
   it('renders roles and descriptions', async () => {
     const element = await AboutDevelopersBlock();
     const html = renderToStaticMarkup(<TestProviders>{element}</TestProviders>);
-
     expect(html).toContain('developers.stardustmeg.role');
     expect(html).toContain('developers.ripetchor.role');
     expect(html).toContain('developers.zagorky.role');
-
     expect(html).toContain('developers.stardustmeg.description');
     expect(html).toContain('developers.ripetchor.description');
     expect(html).toContain('developers.zagorky.description');
