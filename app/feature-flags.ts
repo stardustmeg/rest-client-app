92a7f6bc284080842d0e6bdfdb526f5be802a46b
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-09-05 21:29:51 +0500

    chore: on ff

--- app/feature-flags.ts
+++ app/feature-flags.ts
@@ -4,7 +4,7 @@ const FEATURE_FLAGS = {
   languageSelect: true,
   signUpForm: true,
   signInForm: true,
-  restClient: false,
+  restClient: true,
 } as const;
 
 export const enabled = (feature: FeatureFlag) => FEATURE_FLAGS[feature];
