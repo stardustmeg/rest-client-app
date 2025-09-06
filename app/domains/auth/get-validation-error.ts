28fd824c90da775a5c3e26544f1943cc84052e4b
zagorky <zagorkiynahyi@gmail.com>
2025-09-06 20:05:46 +0300

    feat: add passwordField component

--- app/domains/auth/get-validation-error.ts
+++ app/domains/auth/get-validation-error.ts
@@ -1,7 +1,5 @@
 import type { useTranslations } from 'next-intl';
 
-export type FormMessage = Parameters<ReturnType<typeof useTranslations<'form'>>>[0];
-
 type ValidationMessage = Parameters<ReturnType<typeof useTranslations<'validation'>>>[0];
 
 export function getValidationError(
