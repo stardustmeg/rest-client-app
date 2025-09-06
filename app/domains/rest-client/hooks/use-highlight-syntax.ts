4a8a5d322d53e7b94456f116db5765f52c931332
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-09-06 22:37:19 +0500

    refactor: make highlightSyntax sync

--- app/domains/rest-client/hooks/use-highlight-syntax.ts
+++ app/domains/rest-client/hooks/use-highlight-syntax.ts
@@ -1,8 +1,49 @@
 import { useAtomValue } from 'jotai';
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
-import { highlightSyntax } from '@/app/server-actions/server-actions';
+import { useLayoutEffect, useState } from 'react';
+import { type BundledLanguage, createHighlighter } from 'shiki';
+
 import { codeGenLanguageAtom } from '../atoms';
+
+const highlighter = await createHighlighter({
+  themes: ['github-dark', 'github-light'],
+  langs: [
+    'csharp',
+    'dart',
+    'go',
+    'http',
+    'java',
+    'javascript',
+    'kotlin',
+    'c',
+    'objective-c',
+    'ocaml',
+    'php',
+    'powershell',
+    'python',
+    'r',
+    'ruby',
+    'rust',
+    'shell',
+    'swift',
+  ],
+});
+
+function highlightSyntax(code: string, language: string, currentTheme: string): string {
+  let lang: BundledLanguage;
+
+  if (language === 'nodejs') {
+    lang = 'javascript';
+  } else if (language === 'curl') {
+    lang = 'bash';
+  } else {
+    lang = language as BundledLanguage;
+  }
+
+  const theme = currentTheme === 'dark' ? 'github-dark' : 'github-light';
+
+  return highlighter.codeToHtml(code, { lang, theme });
+}
 
 export function useHighlightSyntax(code: string): string {
   const { resolvedTheme } = useTheme();
@@ -10,8 +51,8 @@ export function useHighlightSyntax(code: string): string {
 
   const [highlightedCode, setHighlightedCode] = useState('');
 
-  useEffect(() => {
-    highlightSyntax(code, language, resolvedTheme ?? '').then(setHighlightedCode);
+  useLayoutEffect(() => {
+    setHighlightedCode(highlightSyntax(code, language, resolvedTheme ?? 'dark'));
   }, [code, language, resolvedTheme]);
 
   return highlightedCode;
--- app/server-actions/server-actions.ts
+++ app/server-actions/server-actions.ts
@@ -2,7 +2,6 @@
 
 import pcg from 'postman-code-generators';
 import sdk from 'postman-collection';
-import { type BundledLanguage, codeToHtml } from 'shiki';
 import type { GenerateCodeSnippetParams } from './types';
 
 export async function getLanguageList() {
@@ -33,24 +32,4 @@ export async function generateCodeSnippet({
       }
     });
   });
-}
-
-export async function highlightSyntax(
-  code: string,
-  language: string,
-  currentTheme: string,
-): Promise<string> {
-  let lang: BundledLanguage;
-
-  if (language === 'nodejs') {
-    lang = 'javascript';
-  } else if (language === 'curl') {
-    lang = 'bash';
-  } else {
-    lang = language as BundledLanguage;
-  }
-
-  const theme = currentTheme === 'dark' ? 'github-dark' : 'github-light';
-
-  return await codeToHtml(code, { lang, theme });
 }
