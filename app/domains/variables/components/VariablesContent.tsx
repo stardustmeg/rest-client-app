21cf4a307c46bb00cf1e3c5ae5335e9b108c3d3d
zagorky <zagorkiynahyi@gmail.com>
2025-09-06 18:06:44 +0300

    refactor: refactor skeleton

--- app/domains/variables/components/VariablesContent.tsx
+++ app/domains/variables/components/VariablesContent.tsx
@@ -12,51 +12,53 @@ export const VariablesContent = () => {
     deleteVariable(id);
   };
 
-  return variables.length > 0 ? (
-    <Skeleton loading={isLoading}>
-      <Table.Root size="lg">
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeader>{t('key')}</Table.ColumnHeader>
-            <Table.ColumnHeader textAlign="center">{t('name')}</Table.ColumnHeader>
-            <Table.ColumnHeader textAlign="center">{t('value')}</Table.ColumnHeader>
-            <Table.ColumnHeader textAlign="end">
-              <IconButton
-                size="sm"
-                onClick={deleteAllVariables}
-                aria-label={t('deleteAll')}
-                variant="surface"
-              >
-                <BsTrash3 />
-              </IconButton>
-            </Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {variables.map((item) => (
-            <Table.Row key={item.id}>
-              <Table.Cell>{item.id}</Table.Cell>
-              <Table.Cell textAlign="center">{item.name}</Table.Cell>
-              <Table.Cell textAlign="center">{item.value}</Table.Cell>
-              <Table.Cell textAlign="end">
+  return (
+    <Skeleton loading={isLoading} minH="300px">
+      {variables.length > 0 ? (
+        <Table.Root size="lg">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeader>{t('key')}</Table.ColumnHeader>
+              <Table.ColumnHeader textAlign="center">{t('name')}</Table.ColumnHeader>
+              <Table.ColumnHeader textAlign="center">{t('value')}</Table.ColumnHeader>
+              <Table.ColumnHeader textAlign="end">
                 <IconButton
-                  aria-label={t('delete')}
                   size="sm"
-                  onClick={() => handleDelete(item.id)}
+                  onClick={deleteAllVariables}
+                  aria-label={t('deleteAll')}
+                  variant="surface"
                 >
                   <BsTrash3 />
                 </IconButton>
-              </Table.Cell>
+              </Table.ColumnHeader>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+          </Table.Header>
+          <Table.Body>
+            {variables.map((item) => (
+              <Table.Row key={item.id}>
+                <Table.Cell>{item.id}</Table.Cell>
+                <Table.Cell textAlign="center">{item.name}</Table.Cell>
+                <Table.Cell textAlign="center">{item.value}</Table.Cell>
+                <Table.Cell textAlign="end">
+                  <IconButton
+                    aria-label={t('delete')}
+                    size="sm"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    <BsTrash3 />
+                  </IconButton>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      ) : (
+        <Flex justify="center">
+          <Text textStyle="2xl" fontWeight="bold">
+            {t('noVariables')} 🥲
+          </Text>
+        </Flex>
+      )}
     </Skeleton>
-  ) : (
-    <Flex justify="center">
-      <Text textStyle="2xl" fontWeight="bold">
-        {t('noVariables')} 🥲
-      </Text>
-    </Flex>
   );
 };
