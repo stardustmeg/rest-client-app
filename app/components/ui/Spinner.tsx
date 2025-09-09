f88a68458466f1dbdac8890fa92a3429edd34d0c
zagorky <zagorkiynahyi@gmail.com>
2025-09-09 21:47:01 +0300

    refactor: refactor spinner

--- app/components/ui/Spinner.tsx
+++ app/components/ui/Spinner.tsx
@@ -1,5 +1,6 @@
 import { Text } from '@chakra-ui/react';
 import type { ReactNode } from 'react';
+import Image from "next/image";
 
 const characters = [
   's',
@@ -14,6 +15,8 @@ const characters = [
   'e',
   'g',
   ' ',
+  '✨',
+  ' ',
   'z',
   'a',
   'g',
@@ -21,6 +24,8 @@ const characters = [
   'r',
   'k',
   'y',
+  ' ',
+  '🥲',
   ' ',
   'r',
   'i',
@@ -32,18 +37,20 @@ const characters = [
   'o',
   'r',
   ' ',
+  '💃',
+  ' ',
 ];
 
 export const Spinner = ({ message }: { message?: ReactNode }) => (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4" data-testid="spinner">
+    <div className="flex flex-col place-self-center items-center justify-center gap-4" data-testid="spinner">
       <div className="relative flex h-80 w-80 items-center justify-center">
         {characters.map((char, index) => {
           const angle = (index / characters.length) * 360;
-          const radius = 4;
+          const radius = 6;
           return (
             <span
               key={index}
-              className="absolute top-1/2 left-1/2 font-bold font-mono text-md"
+              className="absolute top-1/2 left-1/2 !font-bold !font-mono !text-md"
               style={{
                 transform: `translate(-50%, -50%) rotate(${angle}deg) translateY(-${radius}rem)`,
                 transformOrigin: 'center center',
@@ -55,7 +62,7 @@ export const Spinner = ({ message }: { message?: ReactNode }) => (
         })}
 
         <div className="!text-8xl -translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 animate-spin">
-          🥸
+          <Image src='/sasa.png' alt='sasarik real photo' width={512} height={512} />
         </div>
       </div>
       {message && <Text data-testid="spinner-message">{message}</Text>}
