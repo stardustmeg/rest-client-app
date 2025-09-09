2ffee498abbf13d3ee4d3dc3e2e577541c58b8a1
zagorky <zagorkiynahyi@gmail.com>
2025-09-09 23:47:16 +0300

    refactor: use convex components

--- app/components/ui/NavigationButtons.tsx
+++ app/components/ui/NavigationButtons.tsx
@@ -2,20 +2,9 @@
 import { Button, Flex, Skeleton } from '@chakra-ui/react';
 import { Authenticated, Unauthenticated } from 'convex/react';
 import { useTranslations } from 'next-intl';
-import { routes } from '@/app/[locale]/routes';
 import { useAuth } from '@/app/hooks/use-auth';
+import { authButtons, navigationButtons } from '@/data/navLinksInfo';
 import { Link } from '@/i18n/routing';
-
-const authButtons = [
-  { title: routes.signIn.translationKey, route: routes.signIn.path },
-  { title: routes.signUp.translationKey, route: routes.signUp.path },
-] as const;
-
-const navigationButtons = [
-  { title: routes.restClient.translationKey, route: routes.restClient.path },
-  { title: routes.historyAndAnalytics.translationKey, route: routes.historyAndAnalytics.path },
-  { title: routes.variables.translationKey, route: routes.variables.path },
-] as const;
 
 export type ButtonListType = 'authButtons' | 'navigationButtons';
 
