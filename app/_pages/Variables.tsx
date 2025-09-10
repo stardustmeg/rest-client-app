024f8fd44025b5191f1dbbf0a54728aca24b58f5
zagorky <zagorkiynahyi@gmail.com>
2025-09-10 09:43:16 +0300

    refactor: replace get translations with use translations

--- app/_pages/Variables.tsx
+++ app/_pages/Variables.tsx
@@ -2,12 +2,10 @@ import { Enabled } from '@/app/components/ui/Enabled';
 import { PageWrapper } from '@/app/components/ui/PageWrapper';
 import { Variables } from '@/app/domains/variables/Variables';
 
-export const VariablesPage = () => {
-  return (
-    <PageWrapper>
-      <Enabled feature="variables">
-        <Variables />
-      </Enabled>
-    </PageWrapper>
-  );
-};
+export const VariablesPage = () => (
+  <PageWrapper>
+    <Enabled feature="variables">
+      <Variables />
+    </Enabled>
+  </PageWrapper>
+);
