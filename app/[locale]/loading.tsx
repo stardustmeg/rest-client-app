bda00623ac82fb4d8043862cd0c59e6d2bc945cc
zagorky <zagorkiynahyi@gmail.com>
2025-09-10 13:53:39 +0300

    feat: add loading to root page

---
+++ app/[locale]/loading.tsx
@@ -0,0 +1,5 @@
+import { Spinner } from '@/app/components/ui/Spinner';
+
+const Loading = () => <Spinner />;
+
+export default Loading;
--- app/components/ui/Spinner.tsx
+++ app/components/ui/Spinner.tsx
@@ -1,6 +1,6 @@
 import { Text } from '@chakra-ui/react';
-import type { ReactNode } from 'react';
 import Image from "next/image";
+import type { ReactNode } from 'react';
 
 const characters = [
   's',
@@ -42,7 +42,7 @@ const characters = [
 ];
 
 export const Spinner = ({ message }: { message?: ReactNode }) => (
-    <div className="flex flex-col place-self-center items-center justify-center gap-4" data-testid="spinner">
+    <div className="flex flex-col items-center justify-center gap-4 place-self-center" data-testid="spinner">
       <div className="relative flex h-80 w-80 items-center justify-center">
         {characters.map((char, index) => {
           const angle = (index / characters.length) * 360;
@@ -50,18 +50,18 @@ export const Spinner = ({ message }: { message?: ReactNode }) => (
           return (
             <span
               key={index}
-              className="absolute top-1/2 left-1/2 !font-bold !font-mono !text-md"
+              className="!font-bold !font-mono !text-md absolute top-1/2 left-1/2"
               style={{
                 transform: `translate(-50%, -50%) rotate(${angle}deg) translateY(-${radius}rem)`,
                 transformOrigin: 'center center',
               }}
-            >
-  {char}
-</span>
+              >
+              {char}
+            </span>
           );
         })}
 
-        <div className="!text-8xl -translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 animate-spin">
+        <div className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 animate-spin">
           <Image src='/sasa.png' alt='sasarik real photo' width={512} height={512} />
         </div>
       </div>
