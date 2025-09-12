ba8c5f4b9ad3d6d3d6bfc1f9132cb070cddac59d
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-09-12 15:46:13 +0500

    feat: colorize response info

--- app/domains/rest-client/components/ResponseInformation.tsx
+++ app/domains/rest-client/components/ResponseInformation.tsx
@@ -1,5 +1,13 @@
 'use client';
 
+import {
+  isClientErrorResponse,
+  isInformationalResponse,
+  isRedirectionResponse,
+  isServerErrorResponse,
+  isSuccessResponse,
+} from '@/app/lib/utils';
+
 export interface ResponseInformationProps {
   status: number;
   size: number;
@@ -20,14 +28,38 @@ export const ResponseInformation = ({
   return (
     <div data-testid="response-information" className="flex gap-3">
       <span data-testid="response-information-status">
-        {labelStatus ?? 'Status'}: {status}
+        {labelStatus ?? 'Status'}: <span className={getStatusColor(status)}>{status}</span>
       </span>
       <span data-testid="response-information-size">
-        {labelSize ?? 'Size'}: {size}
+        {labelSize ?? 'Size'}: <span className={getStatusColor(status)}>{size}</span>
       </span>
       <span data-testid="response-information-time">
-        {labelTime ?? 'Time'}: {time}
+        {labelTime ?? 'Time'}: <span className={getStatusColor(status)}>{time}</span>
       </span>
     </div>
   );
+};
+
+const getStatusColor = (status: number): string => {
+  if (isInformationalResponse(status)) {
+    return 'text-gray-500';
+  }
+
+  if (isSuccessResponse(status)) {
+    return 'text-green-600';
+  }
+
+  if (isRedirectionResponse(status)) {
+    return 'text-orange-600';
+  }
+
+  if (isClientErrorResponse(status)) {
+    return 'text-red-600';
+  }
+
+  if (isServerErrorResponse(status)) {
+    return 'text-red-600';
+  }
+
+  return 'text-gray-600';
 };
