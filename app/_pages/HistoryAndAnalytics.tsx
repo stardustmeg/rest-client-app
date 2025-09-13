260f348187a527fec019375ef224a98403e6bdfb
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-09-13 17:08:36 +0300

    feat: add page wrapper to history

--- app/_pages/HistoryAndAnalytics.tsx
+++ app/_pages/HistoryAndAnalytics.tsx
@@ -1,5 +1,10 @@
 import { HistoryList } from '../components/ui/HistoryList';
+import { PageWrapper } from '../components/ui/PageWrapper';
 
 export const HistoryAndAnalyticsPage = () => {
-  return <HistoryList />;
+  return (
+    <PageWrapper>
+      <HistoryList />
+    </PageWrapper>
+  );
 };
