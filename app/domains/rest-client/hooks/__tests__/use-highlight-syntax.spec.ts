1a5f972ab592ac73df3211d0541563bd78ce3707
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-09-14 20:50:38 +0500

    fix: tests

--- app/domains/rest-client/hooks/__tests__/use-highlight-syntax.spec.ts
+++ app/domains/rest-client/hooks/__tests__/use-highlight-syntax.spec.ts
@@ -2,12 +2,12 @@ import { renderHook } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { useHighlightSyntax } from '../use-highlight-syntax';
 
-describe.skip(useHighlightSyntax.name, () => {
+describe(useHighlightSyntax.name, () => {
   it('should return highlighted code', () => {
     const { result } = renderHook(() => useHighlightSyntax('csharp', 'code'));
 
     expect(result.current).toBe(
-      '<pre class="shiki github-dark" style="background-color:#24292e;color:#e1e4e8" tabindex="0"><code><span class="line"><span>code</span></span></code></pre>',
+      '<pre class="shiki github-dark" style="background-color:#24292e;color:#e1e4e8" tabindex="0"><code><span class="line"><span style="color:#E1E4E8">code</span></span></code></pre>',
     );
   });
 });
