f41d5b0e4d09f118e3b330295e92b3e0f332fd4b
zagorky <zagorkiynahyi@gmail.com>
2025-09-14 13:03:26 +0300

    refactor: replace `Link` with `Button` in NavigationLink and simplify routing logic

--- app/components/ui/NavigationLink.tsx
+++ app/components/ui/NavigationLink.tsx
@@ -1,7 +1,7 @@
-import { Link } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 import { useTranslations } from 'next-intl';
 import type { RouteConfig } from '@/app/[locale]/routes';
-import { Link as I18nLink } from '@/i18n/routing';
+import { Link } from '@/i18n/routing';
 
 interface NavLinkProps {
   route: RouteConfig;
@@ -12,16 +12,14 @@ export const NavigationLink = ({ route, direction = 'horizontal' }: NavLinkProps
   const t = useTranslations('navigation');
 
   return (
-    <Link
+    <Button
+      variant="ghost"
       asChild
-      py={2}
       w="max-content"
       fontWeight="medium"
       textAlign={direction === 'vertical' ? 'center' : 'left'}
     >
-      <I18nLink href={route.path}>
-        <span>{t(route.translationKey)}</span>
-      </I18nLink>
-    </Link>
+      <Link href={route.path}>{t(route.translationKey)}</Link>
+    </Button>
   );
 };
