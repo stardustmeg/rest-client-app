98b6966f266228b9bce029da60840200d46aa9e3
Margarita Golubeva <margaret.golubeva@gmail.com>
2025-09-19 02:38:33 +0300

    feat: update ls

--- app/hooks/use-local-storage.ts
+++ app/hooks/use-local-storage.ts
@@ -2,12 +2,12 @@ import { useCallback, useRef, useSyncExternalStore } from 'react';
 
 const LS_PREFIX = 'no_no_no_mister_fish';
 
-export const getFullKey = (key: string): string => `${LS_PREFIX}_${key}`;
+const getFullKey = (key: string): string => `${LS_PREFIX}_${key}`;
 
 type SetValue<T> = (value: T | ((prev: T) => T)) => void;
-type UseLocalStorageReturn<T> = [T, SetValue<T>];
 
-export const useLocalStorage = <T>(key: string, initialValue: T): UseLocalStorageReturn<T> => {
+export const useLocalStorage = <T>(key: string, initialValue: T): [T, SetValue<T>] => {
+  const fullKey = getFullKey(key);
   const lastLocalStorageValue = useRef(initialValue);
 
   const subscribe = useCallback(
@@ -24,7 +24,6 @@ export const useLocalStorage = <T>(key: string, initialValue: T): UseLocalStorag
   );
 
   const getSnapshot = () => {
-    const fullKey = getFullKey(key);
     const value = localStorage.getItem(fullKey);
     if (!value) {
       return initialValue;
@@ -35,9 +34,7 @@ export const useLocalStorage = <T>(key: string, initialValue: T): UseLocalStorag
     return lastLocalStorageValue.current;
   };
 
-  const getServerSnapshot = () => {
-    return initialValue;
-  };
+  const getServerSnapshot = () => initialValue;
 
   const value = useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
 
@@ -45,7 +42,6 @@ export const useLocalStorage = <T>(key: string, initialValue: T): UseLocalStorag
     const newValue =
       typeof v === 'function' ? (v as (prev: T) => T)(lastLocalStorageValue.current) : v;
     const valueToStore = JSON.stringify(newValue);
-    const fullKey = getFullKey(key);
     localStorage.setItem(fullKey, valueToStore);
     lastLocalStorageValue.current = newValue;
     window.dispatchEvent(new StorageEvent('storage', { key: fullKey }));
